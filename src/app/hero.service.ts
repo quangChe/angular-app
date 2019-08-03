868095c7c4fae7d1d84cee930b02511716dd631d
quangChe <quang.n.che@gmail.com>
2019-08-03 16:59:40 -0700

    Imported and used message service inside of hero service

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -2,13 +2,17 @@ import { Injectable } from '@angular/core';
 import { Hero } from './hero';
 import { HEROES } from './mock-heroes';
 import { Observable, of } from 'rxjs';
+import { MessageService } from './message.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class HeroService {
 
+  constructor(private messageService: MessageService) {}
+
   getHeroes(): Observable<Hero[]> {
+    this.messageService.add('HeroServide: fetched heroes');
     return of(HEROES);
   }
 }
